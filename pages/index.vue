b598decf744caf1647511b642c7a576e4648a355
rileysiu <rileysiu23@gmail.com>
2024-10-24 11:59:12 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <Button>Click</Button>
+    </div>
+</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <Button>Users Click</Button>
+    </div>
+</template>
